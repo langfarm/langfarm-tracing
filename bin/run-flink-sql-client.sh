ea535f02eba8e5f0494bcd26d55ac7b66f5e1835
chenlb <chenlbphoto@163.com>
2025-01-12 11:56:18 +0800

    build: flink-sql-client 启动的 docker-compose-flink-sql-client.yml 换位置。

--- bin/run-flink-sql-client.sh
+++ bin/run-flink-sql-client.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-mkdir -p /tmp/langfarm-tracing
-docker compose -f docker-compose-flink-sql-client.yml run --rm sql-client /bin/bash
+mkdir -p /tmp/langfarm/flink
+mkdir -p /tmp/langfarm/paimon
+docker compose -f docker/docker-compose-flink-sql-client.yml run --rm sql-client /bin/bash
