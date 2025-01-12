5d8b8587165e7b1a14a512dbb87d9671987726f6
chenlb <chenlbphoto@163.com>
2025-01-12 11:55:07 +0800

    build: docker-compose.yml 包括 langfarm-tracing 的 docker

--- bin/start-langfarm-tracing-with-infra.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-docker compose -f docker/docker-compose-with-infra.yml -p langfarm up -d
--- bin/start-only-infra-docker-compose.sh
+++ bin/start-only-infra-docker-compose.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-docker compose -f docker/docker-compose-only-infra.yml -p langfarm up -d
+# 如果没有 langfarm_default 先创建
+# docker network create langfarm_default
+docker compose -f docker/docker-compose-only-infra.yml -p langfarm-infra up -d
